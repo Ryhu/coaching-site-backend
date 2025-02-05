d68e78f9808a87da01f07618f46d38ff1d96a6e7
Ryan <ryanhuuu@gmail.com>
2025-02-05 17:48:40 -0500

    update models

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,5 +1,8 @@
 class Coach < ApplicationRecord
   include ActiveModel::Serialization
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true
     
   has_many :lessons
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,6 @@
 class Lesson < ApplicationRecord
   include ActiveModel::Serialization
+  validates :first_name, presence: true
 
   belongs_to :coach, foreign_key: "coachId"
   belongs_to :student, foreign_key: "studentId", optional: true
